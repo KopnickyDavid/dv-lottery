20462fde8622b36d68e2a9c0fb145222b4a3d7f7
DV Studio <72212575+DvScriptsSK@users.noreply.github.com>
2022-06-12 15:22:37 +0200

    Update client_config.lua

--- config/client_config.lua
+++ config/client_config.lua
@@ -1,40 +1,30 @@
 Config = {}
-----FRAMEWORK----
-Config.vehicle = 'qb' -- qb/esx
-Config.boat = 0x3FD5AA2F --use only for QB
-Config.progressbar = 'qb' --qb/esx progressbar type
-----ALERT----
-Config.successpdtime = 120000--Time to notify pd after success hacking in ms
-Config.failpdtime = 60000 --Time to notify pd after failed hacking in ms
-Config.moneyprop = `hei_prop_hei_cash_trolly_01` -- prop for money trolley
-----COORDS/DISTANCE----
-Config.distance = 3 -- distance to see marker and text
-Config.Locations = {
-  Start = vector3(131.76, -1184.77, 28.5),
-  Boat = vector3(-1186.53, -1021.04, 1.17),
-  Yacht = vector3(-2018.84, -1040.0, 1.45),
-  Ending = vector3(-2085.7, -1018.02, 12.1),
-  hacking = vector3(-2031.19, -1038.06, 2),
-  Collect = {
-    vector3(-2094.4231, -1008.6356, 4.8843), --money
-    vector3(-2071.4775, -1019.6768, 4.8841), --money
-  }
+Config.framework = 'qb' -- esx/qb
+Config.distance = 2 --distance to show markers/drawtext
+Config.usekey = 38  -- key to open stashes/bossmenu  default E you can change to other https://docs.fivem.net/docs/game-references/controls/
+Config.load = 'QBCore:Client:OnPlayerLoaded' -- 'QBCore:Client:OnPlayerLoaded'/'esx:playerLoaded'
+Config.markers = {
+    type = 2, --type of marker default arrow you can change to other https://docs.fivem.net/docs/game-references/markers/
+    red =225, -- red colour scale 0-225
+    green = 0,-- green colour scale 0-225
+    blue = 0, -- blue colour scale 0-225
+    scalex =  0.25, -- scale of the marker for x
+    scaley = 0.25,  -- scale of the marker for y
+    scalez = 0.25   -- scale of marker height
 }
-
-----TEXTS----
-Config.progressbartext = { --text for progressbar
-    "Deleting camera files",
-    "Taking money"
+Config.shop = {
+    vector3(25.75, -1346.69, 29.5),
+    vector3(-48.03, -1757.48, 29.42),
+    vector3(373.95, 326.75, 103.57),
+    vector3(-1487.0, -379.32, 40.16),
+    vector3(-1222.74, -907.11, 12.33),
+    vector3(-707.41, -913.25, 19.22),
+    vector3(1135.61, -981.95, 46.42),
+    vector3(1163.56, -323.34, 69.21)
 }
-Config.DrawTexts = {-- texts for drawtext
-"[E] Start heist",
-"[E] Spawn Boat",
-"[E] Rob",
-"[E] Hack",
-"[E] Take money",
-"[E] Delete files",
-}
-Config.blips = { -- blip names
-  "Boat",
-  "Yacht"
+Config.blip = {
+    spirit = 431,
+    scale = 0.5,
+    colour = 25,
+    name = "Lottery shop",
 }
